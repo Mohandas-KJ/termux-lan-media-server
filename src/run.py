7474eb15c47c65e0dd07c3fd1bade69af0fc16ba
Mohan Das <mohandaskj.11@gmail.com>
2026-02-07 22:53:46 +0530

    Add argument check and usage instructions in run.py

--- src/run.py
+++ src/run.py
@@ -7,3 +7,8 @@ import os
 
 # Default path for linux
 TERMUX = "storage/emulated/0/Movies"
+
+# Check if args are passed correctly!
+if len(sys.argv) < 2:
+    print("Usage: python run.py /path/to/your/movies <--termux>")
+    sys.exit(1)
